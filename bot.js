2850c33cd385c13c2f3e38e144296385c3ae787c
Kyle Johnston <kyle9657@gmail.com>
2019-05-13 17:49:12 +0100

    ...

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 var Discord = require('discord.io');
 var logger = require('winston');
-var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -10,7 +9,6 @@ logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
     token: process.env.BOT_TOKEN
-    autorun: true
 });
 bot.on('ready', function (evt) {
     logger.info('Logged in as: MR ROBOTO');
